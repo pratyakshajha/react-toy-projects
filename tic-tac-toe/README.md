cf58e26145f1f0b243d5ca01ebd2541a1e786082
pratyakshajha <34786069+pratyakshajha@users.noreply.github.com>
2024-05-23 23:46:29 +0530

    use for loop to generate grid

--- tic-tac-toe/README.md
+++ tic-tac-toe/README.md
@@ -13,7 +13,7 @@
 
 ## To Do
 - [X] For the current move only, show “You are at move #…” instead of a button.
-- [ ] Rewrite Board to use two loops to make the squares instead of hardcoding them.
+- [X] Rewrite Board to use two loops to make the squares instead of hardcoding them.
 - [ ] When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
 - [ ] Display the location for each move in the format (row, col) in the move history list.
 - [ ] Add a refresh/reset button + play again
--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -25,24 +25,28 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  function getGrid() {
+    const grid = [];
+    for (let i = 0; i < 3; i++) {
+      const squareList = [];
+      for (let j = 0; j < 3; j++) {
+        squareList.push(
+          <Square
+            key={i*3 + j}
+            value={squares[i*3 + j]}
+            onSquareClick={() => handleClick(i*3 + j)}
+          />
+        );
+      }
+      grid.push(<div className="board-row">{squareList}</div>);
+    }
+    return grid;
+  };
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {getGrid()}
     </>
   );
 }
