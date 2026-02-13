339cd519612df3153b65b95b4e938ded2f9f9300
pratyakshajha <pratyakshajha@users.noreply.github.com>
2026-02-13 23:58:59 +0530

    add gif

--- show-data-from-api/src/App.tsx
+++ show-data-from-api/src/App.tsx
@@ -30,6 +30,9 @@ function App() {
         onClick={() => loadJokes()}
         className="absolute top-0 right-0 bg-blue-500 text-white px-4 py-2 m-5 rounded-xl hover:bg-blue-600 transition"
       >Reload</button>
+      <h1 className="text-2xl font-extrabold text-center text-gray-200 mb-15">
+        Flip for a Punchline!
+      </h1>
       <JokeList jokes={data} />
     </>
   )
